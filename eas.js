24a728d74d1a57e62c428b4bffe7b11657b1989c
Brigette <long.brigettee@gmail.com>
2024-01-22 12:06:58 -0800

    Create grid layout
    Usage of CSS grid eliminated need for createRow() function, hence
    deletion of function.
    
    Border styles applied to grid container and items.

--- eas.js
+++ eas.js
@@ -1,21 +1,20 @@
 //functions to create the divs
-function createColumn() {
-    for (i = 0; i < 16; i++) {
+function createBoxes() {
+    for (i = 0; i < 256; i++) {
         let newDiv = document.createElement('div');
-        newDiv.classList.add('column');
+        newDiv.classList.add('boxes');
         let div = document.getElementById('grid-container');
         div.appendChild(newDiv); 
-        createRow() 
     }
 }
 
-function createRow() {
-    for (r = 0; r < 16; r++) {
-        let row = document.createElement('div');
-        row.classList.add('row'); 
-        let column = document.querySelectorAll('.column'); 
-        column[i].appendChild(row);
-    }
-}
+//function createRow() {
+//    for (r = 0; r < 16; r++) {
+//        let row = document.createElement('div');
+  //      row.classList.add('row'); 
+    //    let column = document.querySelectorAll('.column'); 
+      //  column[i].appendChild(row);
+ //   }
+//}
 
-createColumn()
+createBoxes()
