c7bb36ec46145cd71827045e71fdb8b1d8793305
Brigette <long.brigettee@gmail.com>
2024-01-22 13:19:17 -0800

    Add reset button
    Pressing reset button will clear the grid of any colors.

--- eas.js
+++ eas.js
@@ -8,15 +8,26 @@ function createBoxes() {
     }
 }
 
-createBoxes()
+createBoxes();
 
-//add event listeners 
+//add event listeners for hover effect
 
 const boxes = document.querySelectorAll('.boxes'); 
 
 document.addEventListener('mouseover', function(event) {
     if (event.target.className == 'boxes') {
-          let target = event.target;
-          target.classList.add('permahover');
+        let target = event.target;
+        target.classList.add('permaHover');
     }
   });
+
+//reset board function 
+const resetBtn = document.querySelector('.reset'); 
+
+resetBtn.addEventListener('click', event => {
+    boxes.forEach(box => {
+        box.classList.remove('permaHover');
+    })
+})
+
+ 
